60a252e80fab57648d523dce29cd4d39b6bbf62a
Adarsh-Tripathy <tripathyadarsh02@gmail.com>
2026-06-09 17:10:31 +0530

    Create left_rotate_array.cpp

---
+++ Day_15/left_rotate_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int d;
+    cin>>d;
+    reverse(a,a+d);
+    reverse(a+d,a+n);
+    reverse(a,a+n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
